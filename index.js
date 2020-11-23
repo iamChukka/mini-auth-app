a33dc7932fc398dc9c5878ce494414b010ad7169
Chukwuka Eluchie <eluchiechukwuka@yahoo.co.uk>
2020-11-23 13:42:58 +0000

    Chukka's first Commit on First HTML/CSS/JS app

---
+++ index.js
@@ -0,0 +1,57 @@
+console.log('Chuka App Attempt');
+
+//CamelNotation: oneTwoThreeFour
+//Pascal Notation: OneTwoThreeFour
+
+//FACTORY FUNCTIONS
+// function createCircle(radius) {
+//     return {
+//         radius,
+//         draw() {
+//             console.log('draw');
+//         }
+//     };
+// }
+
+// const myCircle = createCircle(1);
+
+// //CONSTRUCTOR FUNCTIONS
+// function Circle(radius) {
+//     this.radius = radius;
+//     this.draw = function(){
+//         console.log('draw');
+//     }
+// }
+
+// const circle = new Circle(1);
+
+// const circle = {
+//     radius: 1,
+//     draw() {
+//         console.log('draw');
+//     }
+// };
+
+// for (let key in circle)
+//     console.log(key, circle[key]);
+    
+// for (let key of Object.keys(circle))
+//     console.log(key);
+
+let username= document.getElementById("uname").value;
+let password = document.getElementById("pword").value;
+
+const form = document.querySelector('form');
+const usernameInput = document.querySelector('#uname');
+const passwordInput = document.querySelector('#pword');
+const submitBtn = document.querySelector('#submit'); 
+
+submitBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (usernameInput.value=="admin"&&passwordInput.value=="online") {
+    console.log("Username: " + usernameInput.value);
+
+    console.log("Password: " + passwordInput.value);
+  }
+})
+
