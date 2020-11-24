39dc2ca8450f601958d596c6f9d0018160deb8c4
Chukwuka Eluchie <eluchiechukwuka@yahoo.co.uk>
2020-11-24 14:15:16 +0000

    Generation of Description and Amount

--- index.js
+++ index.js
@@ -1,53 +1,21 @@
 console.log('Chuka App Attempt');
 
-//CamelNotation: oneTwoThreeFour
-//Pascal Notation: OneTwoThreeFour
-
-//FACTORY FUNCTIONS
-// function createCircle(radius) {
-//     return {
-//         radius,
-//         draw() {
-//             console.log('draw');
-//         }
-//     };
-// }
-
-// const myCircle = createCircle(1);
-
-// //CONSTRUCTOR FUNCTIONS
-// function Circle(radius) {
-//     this.radius = radius;
-//     this.draw = function(){
-//         console.log('draw');
-//     }
-// }
-
-// const circle = new Circle(1);
-
-// const circle = {
-//     radius: 1,
-//     draw() {
-//         console.log('draw');
-//     }
-// };
-
-// for (let key in circle)
-//     console.log(key, circle[key]);
-    
-// for (let key of Object.keys(circle))
-//     console.log(key);
-
-
 let username= document.getElementById("uname").value;
 let password = document.getElementById("pword").value;
 
 const form = document.querySelector('form');
 const usernameInput = document.querySelector('#uname');
 const passwordInput = document.querySelector('#pword');
-const submitBtn = document.querySelector('#submit'); 
+const loginBtn = document.querySelector('#login'); 
 
-submitBtn.addEventListener('click',handler)
+const submitBtn = document.querySelector("#submit");
+const descriptionInput = document.querySelector("#desc");
+const amountInput = document.querySelector("#amount");
+
+let key = 0;
+
+
+loginBtn.addEventListener('click', handler)
 
 function handler(e) {
   e.preventDefault();
@@ -58,13 +26,66 @@ function handler(e) {
     
     document.getElementById("myTable").style.display = "flex";
     document.getElementById("myForm").style.display = "flex";
-    document.getElementById("login").style.display = "none";
+    document.getElementById("loginPage").style.display = "none";
 
   }
   else { 
       alert("Wrong Username or Password ");
   }
 }
+
+submitBtn.addEventListener('click', submission)
+
+function submission(e) { 
+  e.preventDefault();
+//  console.log("Description: " +descriptionInput.value);
+//  console.log("Amount: " + amountInput.value);
+  if (descriptionInput.value ==''|| amountInput.value ==''||amountInput.value<0) { 
+    return alert("Enter Valid Description and Amount");
+  }
+  key++;
+  
+  const table = document.getElementById("table");
+
+  let row = table.insertRow();
+  let serial = row.insertCell(0);
+  serial.innerHTML = key;
+  let description = row.insertCell(1);
+  description.innerHTML = descriptionInput.value;
+  let amount = row.insertCell(2);
+  amount.innerHTML = amountInput.value;
+  let editBtn = row.insertCell(3);
+  let deleteBtn = row.insertCell(4);
+
+  // creating button element  
+  let edit = document.createElement('BUTTON');
+  let del = document.createElement('BUTTON');
+                  
+  // creating text to be 
+  //displayed on button 
+  let editText = document.createTextNode("Edit"); 
+  let deleteText = document.createTextNode("Delete");
+  // appending text to button 
+  edit.appendChild(editText); 
+  del.appendChild(deleteText);
+    
+  // appending button to div 
+  editBtn.appendChild(edit);
+  deleteBtn.appendChild(del);
+
+  descriptionInput.value = '';
+  amountInput.value = '';
+}
+
+/*
+JS 
+-FOCUS
+-BLUR
+-Change
+-Click
+
+*/
+
 
 // const handler = (e) => {
 //   e.preventDefault();
@@ -82,4 +103,4 @@ function handler(e) {
 //       alert("Wrong Username or Password ");
 //   }
 // }
-
+ 
